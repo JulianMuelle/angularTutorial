e43878dff3ede278ca8d6bf530216d4783998e75
Julian Mueller <Julian.Mueller@treecom.local>
2025-12-04 09:52:38 +0100

    Signals eingefügt

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {DUMMY_USERS} from '../dummy-user';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -12,16 +12,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
 
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]); //signal hat Tracking-Mechanism, der autom. UI updatet bei Änderung
+  imagePath = computed(()=> '../assets/users/' + this.selectedUser().avatar);
 
   //for computed values in the template -> get-Funktion
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  //get imagePath() {
+  //  return 'assets/users/' + this.selectedUser().avatar;
+  //}
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
-    console.log("Button geklickt");
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
